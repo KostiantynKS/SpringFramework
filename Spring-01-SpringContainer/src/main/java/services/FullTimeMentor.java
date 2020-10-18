7fcd37c61e1ddf0ad117cd4d3e06f3559904dd08
kostiantynkuzmenko <konstantinks380@gmail.com>
2020-10-18 13:17:53 -0400

    Spring Container

---
+++ Spring-01-SpringContainer/src/main/java/services/FullTimeMentor.java
@@ -0,0 +1,9 @@
+package services;
+
+import interfaces.Mentor;
+
+public class FullTimeMentor implements Mentor {
+    public void createAccount() {
+        System.out.println("Creating a Full Time Mentor");
+    }
+}
---
+++ Spring-01-SpringContainer/src/main/java/services/PartTimeMentor.java
@@ -0,0 +1,10 @@
+package services;
+
+import interfaces.Mentor;
+
+public class PartTimeMentor implements Mentor {
+    public void createAccount() {
+
+        System.out.println("Creating a Part Time Mentor");
+    }
+}
