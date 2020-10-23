947762eea115927933c75973e03dd05b4f94f3df
kostiantynkuzmenko <konstantinks380@gmail.com>
2020-10-23 15:33:24 -0400

    don't come back to this commit

--- Spring-01-SpringContainer/src/main/java/services/FullTimeMentor.java
+++
@@ -1,9 +0,0 @@
-package services;
-
-import interfaces.Mentor;
-
-public class FullTimeMentor implements Mentor {
-    public void createAccount() {
-        System.out.println("Creating a Full Time Mentor");
-    }
-}
